f78fa9767a2d8b568f81b8daa9d6f23e34659acc
djzib0 <piotrsabatura@gmail.com>
2024-09-12 18:38:08 +0200

    added logic to undo message, added styles

--- src/components/contactForm/ContactForm.tsx
+++ src/components/contactForm/ContactForm.tsx
@@ -4,14 +4,20 @@ import { useEffect, useState } from "react";
 import styles from "./contactForm.module.css";
 import {sayHello} from "@/lib/actions"
 import SectionHeader from "../sectionHeader/SectionHeader";
+// icons import
 import { FaLinkedin } from "react-icons/fa";
 import { FaFacebookMessenger } from "react-icons/fa";
+import { FaRegTrashCan } from "react-icons/fa6";
+import { FaUndo } from "react-icons/fa";
+
 
 const ContactForm = () => {
 
   const [isSent, setIsSent] = useState(false);
-  const [messageLength, setMessageLength] = useState(0);
-  const [formData, setFormData] = useState<any>({
+  const [messageLength, setMessageLength] = useState<number>(0);
+  const [isMessageReseted, setIsMessageReseted] = useState<boolean>(false);
+  const [messageInClipboard, setMessageInClipboard] = useState<string>("");
+  const [formData, setFormData] = useState<{message: string}>({
     message: ""
   })
 
@@ -22,33 +28,38 @@ const ContactForm = () => {
     sayHello();
   }
 
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    const {name, value, type, checked} = e.target
+  function handleChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    const {name, value, type} = e.target
     const newData = {
       ...formData,
-      [name]: type === "checkbox" ? checked : value
+      [name]: value
     }
     setFormData(newData);
+    setMessageLength(newData.message.length)
   }
 
-  const checkMessageLength = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setMessageLength(e.target.value.length)
-  }
-
-  const resetMessage = () => {
-    const newFormData = {messsage: ""}
-    console.log("resetting data")
+  const resetMessage = (e: any) => {
+    setMessageInClipboard(formData.message)
+    setIsMessageReseted(true)
+    const newFormData = {message: ""}
     setFormData(newFormData)
   }
 
+  const undoMessage = () => {
+    setIsMessageReseted(false);
+    const newFormData = {message: messageInClipboard};
+    setFormData(newFormData);
+  }
+
   useEffect(() => {
-    console.log("haha hihi hoho")
+    console.log("refreshing")
   }, [formData])
 
   return (
     <div className={styles.container}>
       <SectionHeader title="Contact me"/>
       <div className={styles.content}>
+
         <div className={styles.contactInformation}>
           <div className={styles.message}>
             <p>
@@ -78,9 +89,10 @@ const ContactForm = () => {
             </div>
           </div>
         </div>
-        <div>
+
+        <div className={styles.formContainer}>
           <p className={styles.formHeader}>Or you can leave me a message via form.</p>
-          <form className={styles.formContainer} action={handleForm}>
+          <form className={styles.form} action={handleForm}>
             <input type="text" placeholder="Your name" name="name" />
             <input type="email" placeholder="Your email" name="email" />
             <textarea 
@@ -90,15 +102,31 @@ const ContactForm = () => {
               maxLength={400}
               value={formData.message}
               />
-            <label className={styles.label}>{messageLength}/400</label>
+            <div className={styles.messageDashboard}>
+              <label className={styles.label}>{messageLength}/400</label>
+              {messageLength > 0 && !isMessageReseted && 
+                <button
+                  onClick={resetMessage}
+                  className={styles.iconButton}
+                  >
+                  <FaRegTrashCan />
+                </button>
+              }
+              {isMessageReseted && 
+                <button 
+                  onClick={undoMessage}
+                  className={styles.iconButton}
+                >
+                  <FaUndo />
+                </button>
+              }
+            </div>
             {!isSent ? 
               <button>Send message</button>
               : <button>Message sent</button>}
-            <button type="reset">Clear</button>
           </form>
-          <button onClick={() => setFormData({message: ""})}>reset message</button>
-          <p>jhkjh</p>
         </div>
+
       </div>
     </div>
   )
